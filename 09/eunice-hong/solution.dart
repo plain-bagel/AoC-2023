a55f32b0dff1cffa7262f30924841070cccd54b2
Eunice Hong <dev@eunice-hong.com>
2023-12-10 01:01:00 +0900

    feat: add solutions for Day 9

--- 09/eunice-hong/solution.dart
+++ 09/eunice-hong/solution.dart
@@ -36,18 +36,55 @@ void printSolutions(String input) {
   print('Part 2: ${solution2(input)}');
 }
 
+final digitRegex = RegExp(r'-*\d+');
+
 /// Solution for Puzzle Part 1
 ///
 /// [input] is the contents of the input file
 String solution1(String input) {
-  // TODO(eunice-hong): My solution here
-  return input;
+  return input
+      .split('\n')
+      .fold(0, (acc, curr) => acc + getExtrapolatedTail(parseHistory(curr)))
+      .toString();
 }
 
 /// Solution for Puzzle Part 2
 ///
 /// [input] is the contents of the input file
 String solution2(String input) {
-  // TODO(eunice-hong): My solution here
-  return input;
+  return input
+      .split('\n')
+      .fold(0, (acc, curr) => acc + getExtrapolatedHead(parseHistory(curr)))
+      .toString();
+}
+
+/// Parses the history of the given [line]
+List<int> parseHistory(String line) => digitRegex
+    .allMatches(line)
+    .map((match) => match.group(0)!)
+    .map((number) => int.tryParse(number) ?? 0)
+    .toList();
+
+/// Returns the extrapolated value of the tail of the given [values]
+int getExtrapolatedTail(List<int> values) {
+  if (values.toSet().length == 1) {
+    return values[0];
+  }
+  final history = List.generate(
+    values.length - 1,
+    (index) => values[index + 1] - values[index],
+  );
+  return values[values.length - 1] + getExtrapolatedTail(history);
+}
+
+/// Returns the extrapolated value of the head of the given [values]
+int getExtrapolatedHead(List<int> values) {
+  if (values.toSet().length == 1) {
+    return values[0];
+  }
+  final history = List.generate(
+    values.length - 1,
+    (index) => values[index + 1] - values[index],
+  );
+  return values[0] - getExtrapolatedHead(history);
 }
