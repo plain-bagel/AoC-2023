ed14927a0b0e3cf08ce9a578bbf5aa1d57b34251
Jeehoon Kang <renardyreveur@gmail.com>
2023-12-14 03:18:55 +0900

    feat: improve upon the previous answer
    - Back to String from Data.Text
    - Not using full search indices, but ending the search as soon as a match is found
    - Single function to handle every case

--- 01/renardyreveur/trebuchet.hs
+++ 01/renardyreveur/trebuchet.hs
@@ -1,69 +1,39 @@
 module Day1 where
 
+import Data.List (isPrefixOf)
 import qualified Data.Map as Map
-import qualified Data.Text as T
-import Data.Text.Internal.Search as Ts (indices)
+import Data.Maybe (fromMaybe)
 import System.IO ()
 
--- Find the first number in a string
+-- Type Aliases
 type Index = Int
-findFirstNumber :: T.Text -> (Index, Int)
-findFirstNumber text = findFirstNumber' text 0
+type Dictionary = Map.Map String Int
+
+-- Dictionaries for numerical words
+numberMap :: Dictionary
+numberMap = Map.fromList [("one", 1), ("two", 2), ("three", 3), ("four", 4), ("five", 5), ("six", 6), ("seven", 7), ("eight", 8), ("nine", 9)]
+
+reverseNumberMap :: Dictionary
+reverseNumberMap = Map.fromList [(reverse k, v) | (k, v) <- Map.toList numberMap]
+
+-- Find the first number in a string
+findFirstNumber :: String -> Dictionary -> Int
+findFirstNumber [] _ = 0
+findFirstNumber (x : xs) dict
+    -- Return as soon as a number or a number word is found
+    | search /= Nothing = fromMaybe 0 $ Map.lookup (fromMaybe "" search) dict
+    | x `elem` ['0' .. '9'] = read [x] :: Int
+    | otherwise = findFirstNumber xs dict -- Otherwise, recurse on the rest of the string
   where
-    findFirstNumber' :: T.Text -> Index -> (Index, Int)
-    findFirstNumber' text index
-        | T.null text = (index, 0)
-        | T.head text `elem` ['0' .. '9'] = (index, read . T.unpack $ T.take 1 text)
-        | otherwise = findFirstNumber' (T.tail text) (index + 1)
+    -- Helper function
+    findKey :: [String] -> String -> Maybe String
+    findKey [] _ = Nothing
+    findKey (k : ks) s
+        | k `isPrefixOf` s = Just k
+        | otherwise = findKey ks s
 
--- Find the last number in a string
-findLastNumber :: T.Text -> (Index, Int)
-findLastNumber = findFirstNumber . T.reverse
-
--- Part 2 --
--- Define numerical words map
-numberMap :: Map.Map T.Text Int
-numberMap = Map.fromList $ map (\x -> (T.pack . fst $ x, snd x)) [("one", 1), ("two", 2), ("three", 3), ("four", 4), ("five", 5), ("six", 6), ("seven", 7), ("eight", 8), ("nine", 9)]
-
--- Lookup function for numerical words
-lookupNumber :: T.Text -> Int
-lookupNumber word = Map.findWithDefault 0 word numberMap
-
--- Helper functions for finding the first and last number/numerical word in a string
--- A much better way of doing this would be to use a Maybe type, and properly handle it with the minimum and maximum functions
-getFirstElement :: [Int] -> Int
-getFirstElement [] = 100000000
-getFirstElement (x : xs) = x
-
-getLastElement :: [Int] -> Int
-getLastElement [] = 0
-getLastElement xs = last xs
-
--- Find the first number/numerical word in a string
-findFirstNumberOrWord :: T.Text -> Int
-findFirstNumberOrWord text = firstOverall
-  where
-    -- First number that appears in the string
-    firstNumber = findFirstNumber text
-    -- First numerical word that appears in the string
-    firstWord = minimum $ map (\x -> (getFirstElement (Ts.indices x text), lookupNumber x)) $ Map.keys numberMap
-    -- Which one appears first overall
-    firstOverall
-        | fst firstNumber < fst firstWord = snd firstNumber
-        | otherwise = snd firstWord
-
--- Find the last number/numerical word in a string
-findLastNumberOrWord :: T.Text -> Int
-findLastNumberOrWord text = lastOverall
-  where
-    -- Last number that appears in the string, subtract from length of text otherwise it will return the index from the end of the string
-    lastNumber = ((T.length text) - (fst . findLastNumber $ text), snd . findLastNumber $ text)
-    -- Last numerical word that appears in the string
-    lastWord = maximum $ map (\x -> (getLastElement (Ts.indices x text), lookupNumber x)) $ Map.keys numberMap
-    -- Which one appears last overall
-    lastOverall
-        | fst lastNumber > fst lastWord = snd lastNumber
-        | otherwise = snd lastWord
+    -- Search for a number word (as provided by the keys of the dictionary) from the beginning of the string
+    search = findKey (Map.keys dict) (x : xs)
 
 -- Main function
 main :: IO ()
@@ -76,10 +46,10 @@ main = do
     let calibrationList = lines calibrationDocument
 
     -- Map and accumulate results over calibrationList (Part 1)
-    let result = foldl (\acc x -> acc + (10 * (snd . findFirstNumber $ T.pack x)) + (snd . findLastNumber $ T.pack x)) 0 calibrationList
+    let result = foldl (\acc x -> acc + (10 * (findFirstNumber x Map.empty)) + (findFirstNumber (reverse x) Map.empty)) 0 calibrationList
 
     -- Map and accumulate results over calibrationList (Part 2)
-    let snd_result = foldl (\acc x -> acc + (10 * (findFirstNumberOrWord $ T.pack x)) + (findLastNumberOrWord $ T.pack x)) 0 calibrationList
+    let snd_result = foldl (\acc x -> acc + (10 * (findFirstNumber x numberMap)) + (findFirstNumber (reverse x) reverseNumberMap)) 0 calibrationList
 
     -- Print Part 1 solution
     putStrLn $ "The sum of all calibration values are: " ++ (show result) ++ "\n"
